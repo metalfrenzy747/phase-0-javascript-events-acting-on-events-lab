ab67e6b047c7503b12c570563438e8aeffde64b8
metalfrenzy747 <allenjeremy747@gmail.com>
2023-12-30 22:22:54 -0600

    success

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+    let rightNumbers= dodger.style.left.replace("px", "");
+    let left= parseInt(rightNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left= `${left + 10}px`;
+    }
+};
+
+document.addEventListener("keydown", function (e) {
+    if (e.key ==="ArrowLeft") {
+       moveDodgerLeft(); 
+    } else if (e.key ==="ArrowRight") {
+        moveDodgerRight();
+    }
+});
